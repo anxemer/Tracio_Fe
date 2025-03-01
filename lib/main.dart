c19d0398b8c153f90add3389d316cab1574d25c6
minhlocdev <91857852+minhlocdev@users.noreply.github.com>
2025-03-02 00:58:34 +0700

    local notification

--- lib/main.dart
+++ lib/main.dart
@@ -2,12 +2,13 @@ import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:permission_handler/permission_handler.dart';
+import 'package:tracio_fe/common/helper/notification/noti_service.dart';
 import 'package:tracio_fe/core/configs/theme/app_theme.dart';
 import 'package:tracio_fe/firebase_options.dart';
-import 'package:tracio_fe/presentation/home/pages/home.dart';
-import 'package:tracio_fe/presentation/profile/pages/user_profile.dart';
+import 'package:tracio_fe/presentation/map/bloc/tracking_location_bloc.dart';
+import 'package:tracio_fe/presentation/map/pages/library.dart';
 import 'package:tracio_fe/presentation/splash/bloc/splash_cubit.dart';
-import 'package:tracio_fe/presentation/splash/page/splash.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:mapbox_maps_flutter/mapbox_maps_flutter.dart' as mp;
 
@@ -16,7 +17,7 @@ import 'service_locator.dart';
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
-
+  await setup();
   await initializeDependencies();
   runApp(const MyApp());
 }
@@ -24,16 +25,34 @@ void main() async {
 Future<void> setup() async {
   await dotenv.load(fileName: ".env");
   mp.MapboxOptions.setAccessToken(dotenv.env['MAPBOX_ACCESS_TOKEN']!);
+
+  await [
+    Permission.locationAlways,
+    Permission.storage,
+    Permission.accessNotificationPolicy,
+    Permission.notification,
+  ].request();
+  //TODO Permission handler
+
+  await NotiService().initNotification();
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
   const MyApp({super.key});
 
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return BlocProvider(
-      create: (context) => SplashCubit()..appStarted(),
+    return MultiBlocProvider(
+      providers: [
+        BlocProvider(create: (context) => SplashCubit()..appStarted()),
+        BlocProvider(create: (context) => LocationBloc())
+      ],
       child: ScreenUtilInit(
         designSize: Size(720, 1600),
         minTextAdapt: true,
@@ -41,7 +60,7 @@ class MyApp extends StatelessWidget {
         builder: (context, child) => MaterialApp(
           theme: AppTheme.appTheme,
           debugShowCheckedModeBanner: false,
-          home: SplashPage(),
+          home: LibraryPage(),
         ),
       ),
     );
