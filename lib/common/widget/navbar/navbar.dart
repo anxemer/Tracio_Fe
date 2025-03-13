a6a6ec268b496590aa749cbd49b4712866f5c5d4
AnXemer <antmse2404@gmal.com>
2025-03-13 17:11:49 +0700

    merge code

--- lib/common/widget/navbar/navbar.dart
+++ lib/common/widget/navbar/navbar.dart
@@ -4,6 +4,8 @@ import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:google_nav_bar/google_nav_bar.dart';
 import 'package:tracio_fe/core/configs/theme/app_colors.dart';
 
+import '../../../core/configs/theme/app_colors.dart';
+
 class BasicNavbar extends StatefulWidget {
   const BasicNavbar({super.key});
 
